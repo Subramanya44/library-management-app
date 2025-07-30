ae6bd95cee724ab34463a58759bbec161603ce04
Subramanya44 <subramanyahegadegkr@gmail.com>
2025-07-30 11:58:26 +0530

    added admin

--- library-management-app/src/app/admin/dashboard/dashboard.component.ts
+++ library-management-app/src/app/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookService } from '../../core/book.service';
+import { TransactionService } from '../../core/transaction.service';
+import { User } from '../../models/user.model';
+import { Book } from '../../models/book.model';
+import { Transaction } from '../../models/transaction.model';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  totalBooks = 0;
+  totalUsers = 0;
+  totalTransactions = 0;
+  borrowedBooks = 0;
 
+  constructor(
+    private bookService: BookService,
+    private transactionService: TransactionService
+  ) {}
+
+  ngOnInit(): void {
+    const books: Book[] = this.bookService.getBooks();
+    const transactions: Transaction[] = this.transactionService.getAllTransactions();
+    const users: User[] = JSON.parse(localStorage.getItem('users') || '[]');
+
+    this.totalBooks = books.length;
+    this.totalUsers = users.length;
+    this.totalTransactions = transactions.length;
+    this.borrowedBooks = transactions.filter(t => !t.returnDate).length;
+  }
 }
